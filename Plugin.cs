6f136b05588f4feb02bf0f65864b79dc763908a2
William Whatley <wwwhatley@icloud.com>
2024-07-09 22:47:02 -0700

    Initial Commit - VersionLabelPatch

--- Plugin.cs
+++ Plugin.cs
@@ -1,17 +1,39 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using BepInEx.Configuration;
+using EFT.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using Renegade.Core.UI;
 
-namespace Stalkov;
+namespace Renegade.Core;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+[BepInPlugin("com.renegade.core", "Renegade.Core", MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("EscapeFromTarkov.exe")]
+[BepInDependency("com.SPT.custom", BepInDependency.DependencyFlags.HardDependency)] // This is used so that we guarantee to load after spt-custom, that way we can disable its patches
+[BepInDependency("com.SPT.singleplayer", BepInDependency.DependencyFlags.HardDependency)] // This is used so that we guarantee to load after spt-singleplayer, that way we can disable its patches
+[BepInDependency("com.SPT.core", BepInDependency.DependencyFlags.HardDependency)] // This is used so that we guarantee to load after spt-core, that way we can disable its patches
+[BepInDependency("com.SPT.debugging", BepInDependency.DependencyFlags.HardDependency)] // This is used so that we guarantee to load after spt-debugging, that way we can disable its patches
+public class RenegadePlugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
-    private void Awake()
+    public static RenegadePlugin Instance;
+    public static string EFTVersionMajor { get; internal set; }
+
+    protected void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Logger.LogInfo($"===================RENEGADE.CORE {MyPluginInfo.PLUGIN_VERSION}=======================");
+        Logger.LogInfo($"Renegade.Core {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+        Logger.LogInfo($"Please report any bugs on the Github and be sure to include the logs.");
+        Logger.LogInfo($"======================================================");
+
+        new RenegadeVersionLabel_Patch().Enable();
     }
 }
---
+++ UI/Patches/RenegadeVersionLabel_Patch.cs
@@ -0,0 +1,59 @@
+﻿using EFT.UI;
+using System.Reflection;
+using SPT.Reflection.Patching;
+using HarmonyLib;
+using SPT.Common.Http;
+using SPT.Common.Utils;
+using SPT.Custom.Models;
+
+namespace Renegade.Core.UI
+{
+    public class RenegadeVersionLabel_Patch : ModulePatch
+    {
+        private static string versionLabel;
+        private static Traverse versionNumberTraverse;
+        private static string renegadeVersion;
+        private static string officialVersion;
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(VersionNumberClass).GetMethod(nameof(VersionNumberClass.Create), BindingFlags.Static | BindingFlags.Public);
+        }
+
+        [PatchPostfix]
+        internal static void PatchPostfix(string major, object __result)
+        {
+            RenegadePlugin.EFTVersionMajor = major;
+
+            if (string.IsNullOrEmpty(versionLabel))
+            {
+                string json = RequestHandler.GetJson("/singleplayer/settings/version");
+                versionLabel = "Renegade-MPT";
+                Logger.LogInfo($"Server version: {versionLabel}");
+            }
+
+            renegadeVersion = MyPluginInfo.PLUGIN_VERSION;
+            versionLabel = "Renegade-MPT";
+
+            Traverse preloaderUiTraverse = Traverse.Create(MonoBehaviourSingleton<PreloaderUI>.Instance);
+
+            preloaderUiTraverse.Field("_alphaVersionLabel").Property("LocalizationKey").SetValue("{0}");
+
+            versionNumberTraverse = Traverse.Create(__result);
+
+            Logger.LogInfo($"versionNumberTraverse:" + versionNumberTraverse.Field("Major"));
+
+            officialVersion = versionNumberTraverse.Field<string>("Major").Value;
+
+            UpdateVersionLabel();
+        }
+
+        public static void UpdateVersionLabel()
+        {
+            Traverse preloaderUiTraverse = Traverse.Create(MonoBehaviourSingleton<PreloaderUI>.Instance);
+
+            preloaderUiTraverse.Field("string_2").SetValue($"{renegadeVersion} | {versionLabel}");
+            versionNumberTraverse.Field("Major").SetValue($"{versionLabel} {renegadeVersion}");
+        }
+    }
+}
